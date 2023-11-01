884bce62377ca32c84e4f2fbdd203a5450bcee5d
Antônio Hugo Ribeiro Pereira Lobo <hugorplobo@gmail.com>
2023-11-01 18:16:01 -0300

    feat: add temporary client comunication

--- server/pokedex_server/src/main.rs
+++ server/pokedex_server/src/main.rs
@@ -1,6 +1,6 @@
 use pokedex_server::network::server::Server;
 
 fn main() {
-    let mut server = Server::new("127.0.0.1:8765");
+    let mut server = Server::new("127.0.0.1:9090");
     server.listen();
 }
