79747c0cc965b07c5d933257e833566be6376976
Antônio Hugo Ribeiro Pereira Lobo <hugorplobo@gmail.com>
2023-11-02 23:40:19 -0300

    refactor: add visibility modifier on message fields

--- server/pokedex_server/src/network/message.rs
+++ server/pokedex_server/src/network/message.rs
@@ -2,9 +2,9 @@ use serde::{Serialize, Deserialize};
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Message {
-    msg_type: i32,
-    request_id: i32,
-    object_reference: String,
-    method_id: String,
-    arguments: String
+    pub msg_type: i32,
+    pub request_id: i32,
+    pub object_reference: String,
+    pub method_id: String,
+    pub arguments: String
 }
