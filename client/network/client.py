8f16ad3fd58b64ec60899afd7ca675cf47b635d1
Antônio Hugo Ribeiro Pereira Lobo <hugorplobo@gmail.com>
2023-11-07 01:40:27 -0300

    feat: add retransmission on timeout

--- client/network/client.py
+++ client/network/client.py
@@ -3,8 +3,16 @@ import socket as sk
 class Client:
     def __init__(self, server_addr: str) -> None:
         self.socket = sk.socket(sk.AF_INET, sk.SOCK_DGRAM)
+        self.socket.settimeout(2.0)
+
         (ip, port) = server_addr.split(":")
         self.server_addr = (ip, int(port))
     
-    def send_message(self, msg: str):
-        self.socket.sendto(msg.encode("utf-8"), self.server_addr)
+    def send_message(self, msg: str) -> bytes:
+        while True:
+            try:
+                self.socket.sendto(msg.encode("utf-8"), self.server_addr)
+                data = self.socket.recv(1024)
+                return data
+            except TimeoutError:
+                continue
--- client/services/pokedex.py
+++ client/services/pokedex.py
@@ -5,9 +5,16 @@ from network.client import Client
 import jsonpickle
 
 class Pokedex:
+    def __init__(self) -> None:
+        self.next_id = 0
+
     def add_pokemon(self, user: User, pokemon: Pokemon):
         args = jsonpickle.encode({ "user": user, "pokemon": pokemon }, unpicklable=False)
-        msg = Message(0, 90, "Pokedex", "add_pokemon", args)
+        msg = Message(0, self.next_id, "Pokedex", "add_pokemon", args)
         json: str = jsonpickle.encode(msg, unpicklable=False)
 
-        Client("127.0.0.1:9090").send_message(json)
+        self.next_id += 1
+
+        data = jsonpickle.decode(Client("127.0.0.1:9090").send_message(json).decode())
+        print(data)
+
