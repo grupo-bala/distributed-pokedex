89a98864520f6fff487c05e5e4dfce5b0209cf84
Henok Teixeira <henokteixeira@alu.ufc.br>
2023-11-08 16:28:59 -0300

    refactor: add do_operation, pack_message, unpack_message methods

--- client/proxy/pokedex.py
+++ client/proxy/pokedex.py
@@ -8,8 +8,34 @@ import jsonpickle
 class Pokedex:
     def add_pokemon(self, user: User, pokemon: Pokemon):
         args = jsonpickle.encode({ "user": user, "pokemon": pokemon }, unpicklable=False)
-        msg = Message(0, id_handler.id(), "Pokedex", "add_pokemon", args)
-        json: str = jsonpickle.encode(msg, unpicklable=False)
-
-        data = jsonpickle.decode(Client("127.0.0.1:9090").send_message(json).decode())
+        
+        data = self.__do_operation("Pokedex", "add_pokemon", args)
+        
         print(data)
+        
+    def __do_operation(self, object_reference: str, method_id: str, args: any):
+        packed_msg, msg_id = self.__pack_message(object_reference, method_id, args)
+        
+        client_udp = Client("127.0.0.1:9090")
+        
+        client_udp.send_message(packed_msg)
+        
+        while True:
+            try:
+                reply = client_udp.recieve_message()
+                
+                unpacked_msg: Message = self.__unpack_message(reply)
+                
+                if unpacked_msg.request_id == msg_id:
+                    return unpacked_msg.arguments
+            except TimeoutError:
+                client_udp.send_message(packed_msg)
+
+    def __pack_message(self, object_reference: str, method_id: str, args: any) -> [bytes, int]:
+        msg_id = id_handler.id()
+        msg = Message(0, msg_id, object_reference, method_id, args)
+        
+        return [jsonpickle.encode(msg, unpickable=False), msg_id]
+    
+    def __unpack_message(self, reply: bytes) -> Message:
+        return jsonpickle.decode(reply.decode())
