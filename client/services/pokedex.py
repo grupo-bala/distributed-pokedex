e4975a31f0f9869a70aa0540be04cf7c6254f8e7
Henok Teixeira <henokteixeira@alu.ufc.br>
2023-11-24 22:09:40 -0300

    feat: add search_pokemon service in pokedex class

--- client/services/pokedex.py
+++ client/services/pokedex.py
@@ -6,3 +6,10 @@ class Pokedex:
     def add_pokemon(self, user: User, pokemon: Pokemon):
         proxy.do_operation("Pokedex", "add_pokemon", { "user": user, "pokemon": pokemon })
         
+    def search_pokemon(self, user: User, pokemon_name: str): 
+        result = proxy.do_operation("Pokedex", "search_pokemon", { "user": user, "name": pokemon_name })
+        
+        if not result:
+            return []
+        
+        return [Pokemon(**pokemon) for pokemon in result]
