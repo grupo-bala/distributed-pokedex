4731e17fdf03cf58f55fcac2db3289a435dd0148
Henok Teixeira <henokteixeira@alu.ufc.br>
2023-11-24 22:10:19 -0300

    feat: add __str__ logic in pokemon class

--- client/model/pokemon.py
+++ client/model/pokemon.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
-from model.pokemon_type import Type
+from model.pokemon_type import Type, color_type
+from termcolor import colored
 
 @dataclass
 class Pokemon:
@@ -12,3 +13,20 @@ class Pokemon:
     special_atk: int
     special_def: int
     speed: int
+    
+    def __str__(self) -> str:
+        bold_text = lambda x: colored(x, attrs=['bold'])
+        
+        header = f"\n{bold_text(self.name.upper())} [{', '.join(map(lambda x: colored(x.lower(), *color_type(x)), self.types))}]"
+        
+        stats = f"""
+HP............{bold_text(self.hp)}
+Attack........{bold_text(self.attack)}
+Defense.......{bold_text(self.defense)}
+Sp. Attack....{bold_text(self.special_atk)}
+Sp. Defense...{bold_text(self.special_def)}
+Speed.........{bold_text(self.speed)}"""
+
+        weakness = f"\nWeakness......{', '.join(map(lambda x: colored(x.lower(), *color_type(x)), self.weakness))}"
+
+        return header + stats + weakness
